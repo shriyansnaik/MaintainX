5a3c1c806cd79bcff69cefb11985b3a7961201db
shriyans <shriyans.n@somaiya.edu>
2023-02-23 23:33:20 +0530

    idk something

--- src/components/CreateRequestScreen.js
+++ src/components/CreateRequestScreen.js
@@ -6,66 +6,58 @@ import {
   Image,
   ScrollView,
 } from 'react-native';
-import React, { useState, useEffect, useContext } from 'react';
-import { CustomText, PriorityButton } from '../components/common';
+import React, {useState, useEffect, useContext} from 'react';
+import {CustomText, PriorityButton} from '../components/common';
 import CustomInput from './common/CustomInput';
 import SelectPriorityButton from './common/SelectPriorityButton';
 import ImagePicker from 'react-native-image-crop-picker';
 import axios from 'axios';
-import { CREATE_TICKET_API, UPLOAD_IMAGE } from '../extras/APIS';
+import storage from '@react-native-firebase/storage';
+import {CREATE_TICKET_API, UPLOAD_IMAGE} from '../extras/APIS';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { GlobalStateContext } from '../routes/GlobalStateProvider';
+import {GlobalStateContext} from '../routes/GlobalStateProvider';
 // const bufferImage = require('buffer-image');
 // import bufferImage from 'buffer-image';
 
-export default function CreateRequestScreen({ navigation, route }) {
+export default function CreateRequestScreen({navigation, route}) {
   const [prioritySelected, setPrioritySelected] = useState('Medium');
   const [title, setTitle] = useState('Flatlist test');
-  const [description, setDescription] = useState('Testing to check if flatlist breaks');
+  const [description, setDescription] = useState(
+    'Testing to check if flatlist breaks',
+  );
   const [assetName, setAssetName] = useState('mouse');
   const [location, setLocation] = useState('area1');
   const [image, setImage] = useState(null);
   const [postFilled, setPostFilled] = useState(false);
-  const { accessToken } = useContext(GlobalStateContext);
+  const {accessToken} = useContext(GlobalStateContext);
 
   useEffect(() => {
     allFieldsFilled();
   }, [title, description, assetName, location]);
 
-  const imageUploadTest = () => {
-    let formData = new FormData();
-    formData.append("image", image);
+  const imageUploadTest = async () => {
+    const uploadUri = image;
+    let filename = uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
 
-    // const params = JSON.stringify({
-    //   image: image,
-    // });
-    // console.log(params);
-    // axios
-    //   .post(UPLOAD_IMAGE, params)
-    //   .then(function (response) {
-    //     console.log(response);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error.message, 'Axios error (create request screen)');
-    //   });
+    const storageRef = storage().ref(`photos/${filename}`);
+    const task = storageRef.putFile(uploadUri);
 
+    task.on('state_changed', taskSnapshot => {
+      console.log(
+        `${taskSnapshot.bytesTransferred} transferred out of ${taskSnapshot.totalBytes}`,
+      );
 
+    });
 
-    console.log(formData);
-    axios({
-      method: 'post',
-      url: UPLOAD_IMAGE,
-      data: formData,
-      headers: { 'Content-Type': 'multipart/form-data' },
-    })
-      .then(function (response) {
-        //handle success
-        console.log(response.data);
-      })
-      .catch(function (response) {
-        //handle error
-        console.log(response);
-      });
+    try {
+      await task;
+
+      const url = await storageRef.getDownloadURL();
+      setImage(null);
+      navigation.goBack();
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const thisPriority = p => {
@@ -107,19 +99,19 @@ export default function CreateRequestScreen({ navigation, route }) {
           'access-token': `${accessToken}`,
         },
       })
-      .then( (response) =>{
-        console.log("nice log",response.data);
+      .then(response => {
+        console.log('nice log', response.data);
         console.log('done');
         // navigation.goBack();
       })
 
-      .catch( (error)=> {
-        console.log(error.message, 'Axios error (create request screen)');
+      .catch(error => {
+        console.log(error, 'Axios error (create request screen)');
         alert('something went wrong (create request screen)');
       });
   };
 
-  const HEADER = ({ navigation }) => {
+  const HEADER = ({navigation}) => {
     return (
       <View
         style={{
@@ -152,7 +144,10 @@ export default function CreateRequestScreen({ navigation, route }) {
           />
         </TouchableOpacity>
 
-        <TouchableOpacity disabled={!postFilled} onPress={() => submitTicket()}>
+        {/* <TouchableOpacity disabled={!postFilled} onPress={() => submitTicket()}> */}
+        <TouchableOpacity
+          disabled={!postFilled}
+          onPress={() => imageUploadTest()}>
           <Image
             source={require('../assets/icons/tick.png')}
             style={{
@@ -185,7 +180,7 @@ export default function CreateRequestScreen({ navigation, route }) {
         </View>
         <View>
           <View
-            style={{ backgroundColor: 'white', padding: 10, marginBottom: 15 }}>
+            style={{backgroundColor: 'white', padding: 10, marginBottom: 15}}>
             <TextInput
               style={styles.textInput}
               onChangeText={value => setTitle(value)}
@@ -230,26 +225,39 @@ export default function CreateRequestScreen({ navigation, route }) {
             </View>
           </View>
           <View
-            style={{ backgroundColor: 'white', padding: 10, marginBottom: 15 }}>
+            style={{backgroundColor: 'white', padding: 10, marginBottom: 15}}>
             <View>
               {/* <TextInput
               style={styles.textInput}
               placeholder="Asset"
               onChangeText={value => setAssetName(value)}
             /> */}
-              <CustomText style={styles.textInput} text={route.params.requestData.asset} textColor={'black'} />
+              <CustomText
+                style={styles.textInput}
+                text={route.params.requestData.asset}
+                textColor={'black'}
+              />
             </View>
             {/* <TextInput style={styles.textInput} placeholder="Category" /> */}
             {/* <TextInput
               placeholder="Location"
               onChangeText={value => setLocation(value) }
             /> */}
-            <CustomText style={styles.textInput} text={route.params.requestData.room + ' , ' + route.params.requestData.subdivision + ' , ' + route.params.requestData.unit} textColor={'black'} />
-
+            <CustomText
+              style={styles.textInput}
+              text={
+                route.params.requestData.room +
+                ' , ' +
+                route.params.requestData.subdivision +
+                ' , ' +
+                route.params.requestData.unit
+              }
+              textColor={'black'}
+            />
           </View>
 
           <View
-            style={{ backgroundColor: 'white', padding: 10, marginBottom: 15 }}>
+            style={{backgroundColor: 'white', padding: 10, marginBottom: 15}}>
             <View
               style={{
                 flexDirection: 'row',
@@ -258,16 +266,16 @@ export default function CreateRequestScreen({ navigation, route }) {
                 justifyContent: 'space-between',
               }}>
               <CustomText text={'Image'} textSize={15} textWeight={600} />
-              <View style={{ flexDirection: 'row' }}>
+              <View style={{flexDirection: 'row'}}>
                 <TouchableOpacity onPress={() => takePhotoFromCamera()}>
                   <Image
-                    style={{ height: 25, width: 25, marginRight: 10 }}
+                    style={{height: 25, width: 25, marginRight: 10}}
                     source={require('../assets/icons/camera.png')}
                   />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => console.log(image)}>
                   <Image
-                    style={{ height: 25, width: 25 }}
+                    style={{height: 25, width: 25}}
                     source={require('../assets/icons/library.png')}
                   />
                 </TouchableOpacity>
@@ -275,10 +283,10 @@ export default function CreateRequestScreen({ navigation, route }) {
             </View>
           </View>
           {image ? (
-            <View style={{ marginBottom: 10, marginLeft: 10 }}>
+            <View style={{marginBottom: 10, marginLeft: 10}}>
               <Image
-                style={{ height: 150, width: 150 }}
-                source={{ uri: image }}></Image>
+                style={{height: 150, width: 150}}
+                source={{uri: image}}></Image>
               <TouchableOpacity
                 style={{
                   position: 'absolute',
@@ -289,7 +297,7 @@ export default function CreateRequestScreen({ navigation, route }) {
                 }}
                 onPress={() => setImage(null)}>
                 <Image
-                  style={{ height: 20, width: 20 }}
+                  style={{height: 20, width: 20}}
                   source={require('../assets/icons/close.png')}
                 />
               </TouchableOpacity>
