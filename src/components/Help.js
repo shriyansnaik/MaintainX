fc01d412f61db2bd7b7eb7a8ca393236f5cb0668
khushi9 <khushi@byldd.com>
2023-03-05 17:22:56 +0530

    settingpage changes

---
+++ src/components/Help.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {CustomText, PriorityButton} from '../components/common';
+export default function Help() {
+  return (
+    <View>
+      <View style={{marginTop:5}}>
+      <CustomText
+            text="How to use Selec app?"
+            textWeight={700}
+            textSize={14}
+            textColor="black"
+         />
+         
+      </View>
+  
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
--- src/extras/APIS.js
+++ src/extras/APIS.js
@@ -1,5 +1,6 @@
+// const address = 'http://192.168.0.107:3000';
 
-const address = 'http://192.168.0.107:3000';
+const address = 'https://selec-maintanence-project-production.up.railway.app';
 export const LOGIN_SCREEN_API = `${address}/login`;
 export const REQUESTS_API = `${address}/technician/ticketdisplay`;
 export const WORK_ORDERS_API = `${address}/technician/workorder`;
@@ -10,3 +11,4 @@ export const UPLOAD_IMAGE = `${address}/technician/upload`;
 export const GET_LOCATIONS=`${address}/admin/locations`;
 
 // accepted (boolean)
+
